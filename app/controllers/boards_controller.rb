40fd9922e0334e3dca7e68a1811314574a3dfc47
ture-shambala <tureshambala4@gmail.com>
2017-04-21 16:19:50 +0530

    added board functionality

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,11 +10,50 @@ class BoardsController < ApplicationController
   end
 
   def create
-  	
+    @board = Board.new(board_params)
+    @board_team = @board.board_teams.new(team_id: params[:team_id])
+    # @board.team_id = params[:team_id]
+    # @board_team = Team.find(params[:team_id])
+    if @board.save && @board_team.save
+      # @board_team.boards << @board
+      redirect_to user_board_path(current_user, @board)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    @board_teams = @board.teams
+    # @board_team = Team.find(params[:team_id])
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+    @teams = Team.all
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    @team = Team.find(params[:team_id])
+    if @board.update(board_params)
+      if @team.present?
+        @board.update(team_id: @team.id)
+      end
+      redirect_to user_board_path(current_user, @board)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to users_path
   end
 
   private
-  # def board_params
-  # 	params.require(:post).permit(:title, :body, category_ids: [])
-  # end
+  def board_params
+  	params.require(:board).permit(:title)
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ActiveRecord::Base
   has_many :team_users
   has_many :users, through: :team_users
   has_many :board_teams
-	has_many :boards, through: :board_teams
+  has_many :boards, through: :board_teams
 end
